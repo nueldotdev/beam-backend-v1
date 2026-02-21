ad4ccc506244d2b6a9983a66e83af93817254633
nueldotdev <walternuel02@gmail.com>
2026-02-21 18:07:59 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const cors = require('cors'); 
+const helmet = require('helmet'); 
+require('dotenv').config(); 
+
+const app = express(); 
+
+app.use(helmet()); 
+app.use(cors()); 
+app.use(express.json()); 
+
+app.get('/api/health', (req, res) => res.json({ status: 'ok' })); 
+
+
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => console.log('API running on port ' + PORT));
